15a5519beb8522fdaa5a00869478c12150057c3a
David Buck <buckdp@gmail.com>
2020-04-25 10:31:40 +1000

    update to use hooks

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Run `npm run start` to start the JSON server and the React client.
 
 ## Versions
 
+- [Version 3](https://github.com/DavidBuck/react-rest-example/tree/v3.0) - Example using hooks and async/await. React version 16.13.1
 - [Version 2](https://github.com/DavidBuck/react-rest-example/tree/v2.0) - Example using a class component and async/await. React version 16.13.1
 - [Version 1](https://github.com/DavidBuck/react-rest-example/tree/v1.0) - Example using a class component and promises. React version 16.13.1.
 
--- src/App.js
+++ src/App.js
@@ -1,32 +1,25 @@
 /* eslint-disable no-console */
 
-import React from "react"
+import React, { useState } from "react"
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      posts: [],
-    }
-  }
+function App() {
+  const [posts, setPosts] = useState([])
 
-  getPosts = async () => {
+  const getPosts = async () => {
     try {
       const response = await fetch("http://localhost:8000/posts")
       if (!response.ok) {
         throw Error(response.statusText)
       }
       const json = await response.json()
-      this.setState({
-        posts: json,
-      })
+      setPosts(json)
       console.log(json)
     } catch (error) {
       console.error(error.message)
     }
   }
 
-  createPost = async () => {
+  const createPost = async () => {
     const url = "http://localhost:8000/posts"
     const data = { id: 4, title: "New Post", postid: 4, author: "New" }
     try {
@@ -41,14 +34,14 @@ class App extends React.Component {
         throw Error(response.statusText)
       }
       const json = await response.json()
-      this.getPosts()
+      getPosts()
       console.log(json)
     } catch (error) {
       console.error(error.message)
     }
   }
 
-  updatePost = async () => {
+  const updatePost = async () => {
     const url = "http://localhost:8000/posts/4"
     const data = {
       title: "Updated Post",
@@ -67,14 +60,14 @@ class App extends React.Component {
         throw Error(response.statusText)
       }
       const json = await response.json()
-      this.getPosts()
+      getPosts()
       console.log(json)
     } catch (error) {
       console.error(error.message)
     }
   }
 
-  deletePost = async () => {
+  const deletePost = async () => {
     const url = "http://localhost:8000/posts/4"
     try {
       const response = await fetch(url, {
@@ -87,88 +80,79 @@ class App extends React.Component {
         throw Error(response.statusText)
       }
       const json = await response.json()
-      this.getPosts()
+      getPosts()
       console.log(json)
     } catch (error) {
       console.error(error.message)
     }
   }
 
-  render() {
-    const { posts } = this.state
-    return (
-      <div>
-        <div className="container mx-auto p-4 m-4 border-solid border-2 border-gray-600 bg-gray-200">
-          <h1 className="text-4xl text-gray-800 py-2 text-center">
-            React Rest Example
-          </h1>
-          <ul className="flex flex-wrap py-4">
-            <li className="mr-3">
-              <button
-                type="button"
-                className="bg-blue-500 hover:bg-blue-700 text-gray-800 font-bold m-1 py-2 px-4 rounded h-10"
-                onClick={this.getPosts}
-              >
-                Get
-              </button>
-            </li>
-            <li className="mr-3">
-              <button
-                type="button"
-                className="bg-blue-500 hover:bg-blue-700 text-gray-800 font-bold m-1 py-2 px-4 rounded h-10 mr-3"
-                onClick={this.createPost}
-              >
-                Post
-              </button>
-            </li>
-            <li className="mr-3">
-              <button
-                type="button"
-                className="bg-blue-500 hover:bg-blue-700 text-gray-800 font-bold m-1 py-2 px-4 rounded
+  return (
+    <div className="container mx-auto p-4 m-4 border-solid border-2 border-gray-600 bg-gray-200">
+      <h1 className="text-4xl text-gray-800 py-2 text-center">
+        React Rest Example
+      </h1>
+      <ul className="flex flex-wrap py-4">
+        <li className="mr-3">
+          <button
+            type="button"
+            className="bg-blue-500 hover:bg-blue-700 text-gray-800 font-bold m-1 py-2 px-4 rounded h-10"
+            onClick={getPosts}
+          >
+            Get
+          </button>
+        </li>
+        <li className="mr-3">
+          <button
+            type="button"
+            className="bg-blue-500 hover:bg-blue-700 text-gray-800 font-bold m-1 py-2 px-4 rounded h-10 mr-3"
+            onClick={createPost}
+          >
+            Post
+          </button>
+        </li>
+        <li className="mr-3">
+          <button
+            type="button"
+            className="bg-blue-500 hover:bg-blue-700 text-gray-800 font-bold m-1 py-2 px-4 rounded
     h-10 mr-3"
-                onClick={this.updatePost}
-              >
-                Put
-              </button>
-            </li>
-            <li className="mr-3">
-              <button
-                type="button"
-                className="bg-blue-500 hover:bg-blue-700 text-gray-800 font-bold m-1 py-2 px-4 rounded h-10 mr-3"
-                onClick={this.deletePost}
-              >
-                Delete
-              </button>
-            </li>
-          </ul>
-          <table className="table-auto py-4 border-gray-500">
-            <thead>
-              <tr className="bg-gray-500">
-                <th className="px-4 py-2 border-gray-600">Id</th>
-                <th className="px-4 py-2 border-gray-600">Title</th>
-                <th className="px-4 py-2 border-gray-600">Author</th>
-              </tr>
-            </thead>
-            <tbody>
-              {posts.map(post => (
-                <tr key={post.id}>
-                  <td className="border px-4 py-2 border-gray-600">
-                    {post.id}
-                  </td>
-                  <td className="border px-4 py-2 border-gray-600">
-                    {post.title}
-                  </td>
-                  <td className="border px-4 py-2 border-gray-600">
-                    {post.author}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
-    )
-  }
+            onClick={updatePost}
+          >
+            Put
+          </button>
+        </li>
+        <li className="mr-3">
+          <button
+            type="button"
+            className="bg-blue-500 hover:bg-blue-700 text-gray-800 font-bold m-1 py-2 px-4 rounded h-10 mr-3"
+            onClick={deletePost}
+          >
+            Delete
+          </button>
+        </li>
+      </ul>
+      <table className="table-auto py-4 border-gray-500">
+        <thead>
+          <tr className="bg-gray-500">
+            <th className="px-4 py-2 border-gray-600">Id</th>
+            <th className="px-4 py-2 border-gray-600">Title</th>
+            <th className="px-4 py-2 border-gray-600">Author</th>
+          </tr>
+        </thead>
+        <tbody>
+          {posts.map(post => (
+            <tr key={post.id}>
+              <td className="border px-4 py-2 border-gray-600">{post.id}</td>
+              <td className="border px-4 py-2 border-gray-600">{post.title}</td>
+              <td className="border px-4 py-2 border-gray-600">
+                {post.author}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
 }
 
 export default App
